8ab5ad6e26ded5c42bcd633cd3fe6016d5a2932e
zeypo <zeypodesign@gmail.com>
2015-09-29 08:32:51 +0200

    [roots] More City like

--- particles/random.roots/index.js
+++ particles/random.roots/index.js
@@ -10,14 +10,19 @@ var Roots = function Roots() {
     var _this = this;
 
     this.config = {
-        length   : 1,
-        velocity : 1,
-        radius   : 10,
-        maxLife  : 100
+        length   : 2,
+        velocity : 100,
+        radius   : 100,
+        maxLife  : 100,
+        clickx   : null,
+        clicky   : null,
+        randomness : 0.99
     };
     this.roots = [];
 
-    this.init = function initCanvas() {
+    this.init = function initCanvas(x, y) {
+        _this.config.clickx = x;
+        _this.config.clicky = y;
         _this.createRoots();
         _this.loop();
     };
@@ -31,7 +36,7 @@ var Roots = function Roots() {
     };
 
     this.render = function renderCanvas() {
-        ctx.fillStyle = 'rgba(0,25,25,0.05)';
+        ctx.fillStyle = 'rgba(0,25,25,0.01)';
         ctx.fillRect(0, 0, c.width, c.height);
 
         for (var i = 0; i < _this.roots.length; i++) {
@@ -43,7 +48,11 @@ var Roots = function Roots() {
 
     this.createRoots = function createRoots() {
         for (var i = 0; i < _this.config.length; i++) {
-            _this.roots.push(new Root(c.width / 2, c.height / 2, i));
+            //var pos = {x : c.width / 2, y : c.height / 2}
+            var pos = {x : _this.config.clickx, y : _this.config.clicky};
+            //var pos = {x : Math.floor((Math.random() + 1) * c.width / 2), y : Math.floor((Math.random() + 1) * c.height / 2)};
+            console.log(pos.x);
+            _this.roots.push(new Root(pos.x, pos.y, i));
         }
     };
 
@@ -59,10 +68,12 @@ var Roots = function Roots() {
             y : this.y
         };
 
-        this.key  = key;
-        this.life = 0;
-        this.rw   = Math.random() * 360;
-        this.vx   = 5;
+        this.stroke = Math.floor((Math.random() * 1) + 1);
+        this.key    = key;
+        this.life   = 0;
+        this.rw     = Math.random() * 360;
+        this.vx     = 5;
+        this.random = _this.config.randomness;
     }
 
     Root.prototype = {
@@ -70,6 +81,7 @@ var Roots = function Roots() {
         draw : function() {
             ctx.beginPath();
             ctx.strokeStyle = '#0ff';
+            ctx.lineWidth = this.stroke;
             ctx.moveTo(this.sp.x, this.sp.y);
             ctx.lineTo(this.ep.x, this.ep.y);
             ctx.stroke();
@@ -80,13 +92,18 @@ var Roots = function Roots() {
         },
 
         update : function() {
-            this.rw   += Math.random() * -0.09;
+
+            var neg = Math.random() < 0.5 ? -1 : 1;
+            this.rw   += Math.random() * neg * ((Math.random() * 0.09) + 0.01);//0.03;
             this.ep.x += Math.cos(this.rw) * this.vx +  Math.random() < 0.5 ? -1 : 1;
             this.ep.y += Math.sin(this.rw) * this.vx +  Math.random() < 0.5 ? -1 : 1;
+            //this.ep.x += Math.floor((Math.random() * 0.1) + 1);
+            //this.ep.y += Math.floor((Math.random() * 0.1) + 1);
             this.life++;
 
-            if (Math.random() > 0.96 && _this.roots.length < 100) {
+            if (Math.random() > this.random && _this.roots.length < 100) {
             	_this.roots.push(new Root(this.sp.x, this.sp.y, _this.roots.length + 1));
+                this.random = this.random - 0.001;
             }
 
             if(this.life >= _this.config.maxLife) {
@@ -115,4 +132,7 @@ var Roots = function Roots() {
 
 
 var roots = new Roots();
-roots.init();
+
+document.addEventListener('click', function(e) {
+    roots.init(e.clientX, e.clientY);
+});
